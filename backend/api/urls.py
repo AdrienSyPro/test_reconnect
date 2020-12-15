242f0f3fe69bdd3c2aa0dbe788f4f8ba70ffecff
AdrienSyPro <adrien.sy.pro@gmail.com>
2020-12-15 19:33:32 +0100

    Ajout rechercheé

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -5,6 +5,8 @@ from rest_framework import routers, serializers, viewsets
 from . import views
 from .models import Beneficiary
 
+from django.shortcuts import get_object_or_404
+from rest_framework.response import Response
 
 # Serializers define the API representation.
 class BeneficiarySerializer(serializers.HyperlinkedModelSerializer):
@@ -19,10 +21,23 @@ class BeneficiaryViewSet(viewsets.ModelViewSet):
     queryset = Beneficiary.objects.all()
     serializer_class = BeneficiarySerializer
 
+    def list(self, request):
+        queryset = Beneficiary.objects.all()
+        serializer = BeneficiarySerializer(queryset, many=True)
+        return Response(serializer.data)
+
+    def retrieve(self, request, pk=None):
+        queryset = Beneficiary.objects.all()
+        beneficiary = get_object_or_404(queryset, name=pk)
+        serializer = BeneficiarySerializer(beneficiary)
+        return Response(serializer.data)
+    # queryset = Beneficiary.objects.all()
+    # serializer_class = BeneficiarySerializer
+
 
 # Routers provide an easy way of automatically determining the URL conf.
 router = routers.DefaultRouter()
-router.register(r'beneficiaries', BeneficiaryViewSet)
+router.register(r'beneficiaries', BeneficiaryViewSet, basename='beneficiary')
 
 urlpatterns = [
     url(r'^', include(router.urls)),
--- client/src/App.js
+++ client/src/App.js
@@ -3,11 +3,13 @@ import "./App.css";
 import names from "./names";
 import { AuthForm } from "./AuthForm";
 import { InfosUser } from "./InfosUser";
+import Search from "./Search";
+import { AddForm } from "./AddForm";
 
 const apiEndpoint = "https://avatars.dicebear.com/v2/avataaars/";
 const apiOptions = "options[mood][]=happy";
 const backendUrl = "http://localhost:8000/";
-const beneficiariesEndpoint = `${backendUrl}api/beneficiaries?format=json`;
+const beneficiariesEndpoint = `${backendUrl}api/beneficiaries/`;
 // const loginEndpoint = `${backendUrl}api/token`;
 
 const getAvatar = name => `${apiEndpoint}${name}.svg?${apiOptions}`;
@@ -18,19 +20,56 @@ function App() {
   );
 
   const [userAuthentified, setUserAuthentified] = React.useState(null);
+  const [localSearch, setLocalSearch] = React.useState('');
 
   const fetchBeneficiaries = async () => {
-    const response = await fetch(beneficiariesEndpoint, {});
+    const response = await fetch(beneficiariesEndpoint + '?format=json', {});
     const json = await response.json();
+
     setRegisteredBeneficiaries(json);
   };
+
+  const fetchOneBeneficiary = async (name) => {
+    const response = await fetch(beneficiariesEndpoint + name, {});
+
+    if (response.status !== 200) {
+      setRegisteredBeneficiaries([]);
+      return;
+    }
+
+    const json = await response.json();
+    setRegisteredBeneficiaries([json]);
+  };
+
+  const headers = new Headers({
+    "Content-Type": "application/json"
+  });
+
+  /**
+   * Add a new beneficiary
+   * @param {string} name 
+   */
+  async function addBeneficiaries(name) {
+    const response = await fetch(beneficiariesEndpoint, {
+      method: "POST",
+      headers: headers,
+      body: JSON.stringify({name: name})
+    });
+
+    fetchBeneficiaries();
+  }
 
   React.useEffect(() => {
     fetchBeneficiaries();
   }, []);
+
   const beneficiaryNames = [...Array(12).keys()].map(number => ({
     name: names[Math.floor(Math.random() * names.length)]
   }));
+
+  function handleLocalSearch(searchText) {
+    setLocalSearch(searchText);
+  }
 
   return (
     <div className="App">
@@ -42,7 +81,9 @@ function App() {
           
         <h1>Bienvenue dans le gestionnaire de bénéficaires Reconnect</h1>
         <hr />
+        <AddForm addFunction={addBeneficiaries}></AddForm>
         <h3>Personnes stockées en base</h3>
+        <Search searchFunction={fetchOneBeneficiary}></Search>
         <div className="Beneficiaries-list">
           {registeredBeneficiaries.map((beneficiary, index) => (
             <div className="Beneficiary-card" key={beneficiary.name + index}>
@@ -53,8 +94,9 @@ function App() {
         </div>
         <hr />
         <h3>Personnes non stockées</h3>
+        <Search searchFunction={handleLocalSearch}></Search>
         <div className="Beneficiaries-list">
-          {beneficiaryNames.map((beneficiary, index) => (
+          {beneficiaryNames.filter(beneficiary => beneficiary.name.includes(localSearch)).map((beneficiary, index) => (
             <div className="Beneficiary-card" key={beneficiary.name + index}>
               <img src={getAvatar(beneficiary.name)} alt={beneficiary.name} />
               <span>{beneficiary.name}</span>
